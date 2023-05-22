7e9f4c385f141fff4ae7a8086d5bc4c0ee5506c9
PatrickSchifter <schiftercorp@outlook.com>
2023-05-22 17:30:42 -0300

    Limpeza dos arquivos padrões do create-react-app

---
+++ frontend/src/routes.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Rotas = () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route />
+            </Routes>
+        </BrowserRouter>
+    )
+}
